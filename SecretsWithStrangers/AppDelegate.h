19c46c1653af1c70169f862af100d2cfdafb5907
Brandon Trebitowski <brandontreb@gmaill.com>
2012-08-11 11:32:55 -0600

    Initial Commit

---
+++ SecretsWithStrangers/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SecretsWithStrangers
+//
+//  Created by Brandon Trebitowski on 8/11/12.
+//  Copyright (c) 2012 Brandon Trebitowski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SecretsWithStrangers/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  SecretsWithStrangers
+//
+//  Created by Brandon Trebitowski on 8/11/12.
+//  Copyright (c) 2012 Brandon Trebitowski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+@end
